909e0dd5f722ec653b9e1b0cef973e248a737de4
Wu Lei <leiw@stu.xidian.edu.cn>
2024-05-21 10:25:40 +0800

    处理返回结果

---
+++ MVC_5/src/com/minis/web/servlet/HandlerMapping.java
@@ -0,0 +1,9 @@
+package com.minis.web.servlet;
+
+import com.minis.web.method.HandlerMethod;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface HandlerMapping {
+	HandlerMethod getHandler(HttpServletRequest request) throws Exception;
+}
---
+++ MVC_5/src/com/test/controller/HelloWorldBean.java
@@ -0,0 +1,46 @@
+package com.test.controller;
+
+import com.minis.beans.factory.annotation.Autowired;
+import com.minis.web.bind.annotation.RequestMapping;
+import com.minis.web.bind.annotation.ResponseBody;
+import com.minis.web.servlet.ModelAndView;
+import com.test.entity.User;
+import com.test.service.BaseService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldBean {
+	@Autowired
+	BaseService baseservice;
+	
+	@RequestMapping("/test2")
+	public void doTest2(HttpServletRequest request, HttpServletResponse response) {
+		String str = "test 2, hello world!";
+		try {
+			response.getWriter().write(str);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@RequestMapping("/test5")
+	public ModelAndView doTest5(User user) {
+		ModelAndView mav = new ModelAndView("test","msg",user.getName());
+		return mav;
+	}
+	@RequestMapping("/test6")
+	public String doTest6(User user) {
+		return "error";
+	}	
+	
+	@RequestMapping("/test7")
+	@ResponseBody
+	public User doTest7(User user) {
+		System.out.println(user.getBirthday());
+		user.setName(user.getName() + "---");
+		//user.setBirthday(new Date());
+		return user;
+	}	
+}
